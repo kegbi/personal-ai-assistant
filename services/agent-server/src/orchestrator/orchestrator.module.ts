e75fc98df26a374cb01158712142e1a9e4599807
Ivan <kegbi.stuff@gmail.com>
2025-10-18 18:04:13 +0200

    Fix import

--- services/agent-server/src/orchestrator/orchestrator.module.ts
+++ services/agent-server/src/orchestrator/orchestrator.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MemoryModule } from '../memory/memory.module';
 import { ToolsModule } from '../tools/tools.module';
+import { CommonModule } from '../common/common.module';
 import { CommandRouter } from './command-router.service';
 import { InputNormalizer } from './input-normalizer.service';
 import { LanggraphModule } from './langgraph/langgraph.module';
@@ -9,7 +10,13 @@ import { OrchestratorService } from './orchestrator.service';
 import { ResponseBuilder } from './response-builder.service';
 
 @Module({
-  imports: [LanggraphModule, MemoryModule, ToolsModule, MessagesModule],
+  imports: [
+    CommonModule,
+    LanggraphModule,
+    MemoryModule,
+    ToolsModule,
+    MessagesModule,
+  ],
   providers: [
     OrchestratorService,
     CommandRouter,
